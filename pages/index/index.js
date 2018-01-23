42da80bd6f8d6971264ffbc8fe8e3df2b707c658
zhaoxiaoguang <zhaoxiaoguang@zuoyebang.com>
2018-01-23 21:27:38 +0800

    列表初始化

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,55 +1,38 @@
 //index.js
 //获取应用实例
+var t = require('../index/indexJson.js');
 const app = getApp()
 
 Page({
   data: {
     motto: '<p>Hello World</p>',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    task: {},
+    hasUserInfo: false
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          console.log(res);
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+    
+  }
+  ,onLoad: function () {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      task: t.task
     })
   }
-})
+  ,onShareAppMessage: function() {
+    return {
+      title: '作业帮一课',
+      path: '/page/index?id=123'
+    }
+  }
+  ,onPullDownRefresh:function () {
+    wx.stopPullDownRefresh();
+  }
+  ,getTaskList: function() {
+
+  }
+  ,btnTo: function(){
+    wx.navigateTo({
+      url: '/pages/calendar/calendar',
+    })
+  }
+});
---
+++ pages/index/indexJson.js
@@ -0,0 +1,67 @@
+var taskList = {
+  "status": 0,
+  "tips": "当前微信未关联任何作业帮账号",
+  "bindUrl": "https://www.jd.com",
+  "list": [
+    {
+      "courseId": "123456",
+      "courseName": "课程名称",
+      "teacherName": "主讲老师",
+      "onlineTime": "上课时间： 12月12日-1月22日",
+      "isFinish": 0,
+      "subject": 3,
+      "subSubject": "英",
+      "T-isFinish": "任务是否已经完成 0未完成，1已经完成"
+    },
+    {
+      "courseId": "678890",
+      "courseName": "课程名称",
+      "teacherName": "主讲老师",
+      "onlineTime": "上课时间： 12月12日-1月22日",
+      "isFinish": 0,
+      "subject": 1,
+      "subSubject": "语",
+      "T-isFinish": "任务是否已经完成 0未完成，1已经完成"
+    },
+    {
+      "courseId": "890123",
+      "courseName": "课程名称",
+      "teacherName": "主讲老师",
+      "onlineTime": "上课时间： 12月12日-1月22日",
+      "isFinish": 0,
+      "subject": 2,
+      "subSubject": "数",
+      "T-isFinish": "任务是否已经完成 0未完成，1已经完成"
+    },
+    {
+      "courseId": "234546",
+      "courseName": "课程名称",
+      "teacherName": "主讲老师",
+      "onlineTime": "上课时间： 12月12日-1月22日",
+      "isFinish": 1,
+      "subject": 3,
+      "subSubject": "英",
+      "T-isFinish": "任务是否已经完成 0未完成，1已经完成"
+    }
+  ],
+  "subjectTextMap": {
+    "1": "语文",
+    "2": "数学",
+    "3": "英语",
+    "4": "物理",
+    "5": "化学",
+    "6": "生物",
+    "7": "政治",
+    "8": "历史",
+    "9": "地理",
+    "10": "兴趣课",
+    "11": "思想品德",
+    "12": "讲座",
+    "13": "理综",
+    "14": "文综"
+  }
+}
+
+module.exports = {
+  task: taskList
+}
